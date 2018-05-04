02d930a32ee6d78820b0a80ab7900aa8e1fd4c55
karolkaras <38975923+karolkaras@users.noreply.github.com>
2018-05-04 11:41:08 +0200

    Add files via upload

---
+++ Log.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Eventing;
+using System.IO;
+
+public class Log
+{
+    public String logSource;
+    public String logMessage;
+    public int eventID;
+    public EventLog eLog;
+    public StreamWriter writetext;
+    public Log(String source, String Message, int id )
+	{
+        this.logMessage = source;
+        this.logSource = Message;
+        this.eventID = id;
+    }
+    
+    public void EventLogWritter()
+    {
+        eLog = new EventLog();
+        eLog.Source = logSource;
+
+        
+        
+            eLog.WriteEntry(logMessage,
+                            System.Diagnostics.EventLogEntryType.Error,
+                            eventID);
+
+
+            eLog.Close();
+        }
+   
+    public void TxtLog()
+    {
+        StreamWriter writetext = new StreamWriter("write.txt");
+        writetext.WriteLine("writing in text file");
+    }   
+           
+        }
+        
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log logo=new Log("logeehher","Hej Gbbe",10101);
+            logo.EventLogWritter();
+        }
+    }
+}
